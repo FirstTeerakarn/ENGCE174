dc12e3e4b706b9fcdf0d131262f4251e9c9d8956
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2024-07-04 20:03:31 +0700

    Delete Lab 1.1.py

--- Lab 1.1.py
+++
@@ -1,8 +0,0 @@
-#List Comprehension
-squares = [x**2 for x in range(10)]
-
-#Dictionary comprehension
-square_dict ={x: x**2 for x in range(5)}
-
-#set comprehension
-even_squares = {x**2 for x in range(10) if x % 2 == 0}
